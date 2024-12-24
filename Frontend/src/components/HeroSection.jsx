37a1603ba6f113e2f7689cd537c47061bf5e026c
Amarjeet <amarjeetofficial81@gmail.com>
2024-12-24 12:36:31 +0530

    hero and some glimps video added

--- Frontend/src/components/HeroSection.jsx
+++ Frontend/src/components/HeroSection.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import style from './HeroSection.module.css'
-// import HeroSectionVideo from '../assets/HeroSectionVideo.mp4'
+import HeroSectionVideo from '../assets/HeroSectionVideo.mp4'
 
 const HeroSection = () => {
   return (
     <div className={style.heroSection}>
-      {/* <video className={style.heroVideo} autoPlay muted loop src={HeroSectionVideo}></video> */}
+      <video className={style.heroVideo} autoPlay muted loop src={HeroSectionVideo}></video>
       <div className={style.videoOverlay}>
         <h1>Clicks Studio: Capturing Moments, Creating Stories</h1>
         <h3>Where Every Frame Speaks a Thousand Words</h3>
