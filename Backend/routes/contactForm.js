02ed6d0df525a5aa097efb2c46d2aa0e08c95ece
Amarjeet <amarjeetofficial81@gmail.com>
2024-12-26 09:08:01 +0530

    contact form and contact form api update

--- Backend/routes/contactForm.js
+++ Backend/routes/contactForm.js
@@ -5,39 +5,44 @@ import 'dotenv/config';
 
 const router = express.Router()
 
-router.post('/', (req, res) => {
-    console.log('req get..')
-    const { name, phone, email, service, budget } = req.body
+router.get('/', (req, res) => {
+    console.log(`req get.. ${req.url}`)
+    // const { name, phone, email, service, budget } = req.body
 
-    const templateParams = {
-        name: name,
-        phone: phone,
-        email: email,
-        service: service,
-        budget: budget,
-    }
+    // const templateParams = {
+    //     name: name,
+    //     phone: phone,
+    //     email: email,
+    //     service: service,
+    //     budget: budget,
+    // }
 
-    emailjs.send(process.env.SERVICE_ID, process.env.TEMPLATE_ID, templateParams, {
-        publicKey: process.env.PUBLIC_KEY_FOR_EMAIL,
-        privateKey: process.env.PRIVATE_KEY_FOR_EMAIL,
-    }).then((response) => {
-        const newFormData = new FormData(templateParams)
-        try {
-            newFormData.save()
-        } catch (error) {
-            console.log(error)
-        }
-        res.send({
-            status: 200,
-            message: 'Data sent successfully.'
-        })
+    // emailjs.send(process.env.SERVICE_ID, process.env.TEMPLATE_ID, templateParams, {
+    //     publicKey: process.env.PUBLIC_KEY_FOR_EMAIL,
+    //     privateKey: process.env.PRIVATE_KEY_FOR_EMAIL,
+    // }).then((response) => {
+    //     const newFormData = new FormData(templateParams)
+    //     try {
+    //         newFormData.save()
+    //     } catch (error) {
+    //         console.log(error)
+    //     }
+    //     res.send({
+    //         status: 200,
+    //         message: 'Data sent successfully.'
+    //     })
+    // })
+    //     .catch((err) => {
+    //         res.send({
+    //             status: 403,
+    //             message: 'Something went wrong.'
+    //         })
+    //     })
+
+    res.send({
+        status:200,
+        message:`request get to ${req.url}`
     })
-        .catch((err) => {
-            res.send({
-                status: 403,
-                message: 'Something went wrong.'
-            })
-        })
 
 })
 
