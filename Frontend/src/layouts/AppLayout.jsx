98d1cf4e369a3dfb0a976d561122e3a8cb5e794a
Amarjeet <amarjeetofficial81@gmail.com>
2024-12-26 10:31:13 +0530

    testing

--- Frontend/src/layouts/AppLayout.jsx
+++ Frontend/src/layouts/AppLayout.jsx
@@ -8,7 +8,7 @@ import { Outlet } from 'react-router-dom'
 const AppLayout = () => {
 
   const fetchToServer = ()=>{
-    fetch('/api',{
+    fetch('/api/',{
       method:'GET',
     }).then(res => res.json())
     .then(data => console.log(data))
